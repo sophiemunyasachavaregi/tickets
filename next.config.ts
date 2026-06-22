92be23eeec8a8e059ca4c1b4ceb8bc3f3a194917
sophiemunyasachavaregi <sophiemunyasachavaregi@gmail.com>
2026-06-22 06:00:44 +0300

    Updated package-lock.json

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,18 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'images.pexels.com' },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.ticketmaster.ph',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 }
